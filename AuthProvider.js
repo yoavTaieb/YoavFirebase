4082d5e6a047ead9c2cb9bd7b000ac16ecd62aaf
yoavTaieb <66865717+yoavTaieb@users.noreply.github.com>
2021-10-08 12:43:27 +0300

    Create AuthProvider.js

---
+++ AuthProvider.js
@@ -0,0 +1,39 @@
+import { useEffect, useState, createContext } from "react";
+import LoadingScreen from "Screens/Loading";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "firebaseConfig";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setPending(false)
+    });
+  }, []);
+
+
+  if (pending) {
+    return (
+      <div style={{
+        height: "100vh"
+      }}>
+        <LoadingScreen />
+      </div>
+    )
+  }
+
+
+  return (
+    <AuthContext.Provider
+      value={{ currentUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
